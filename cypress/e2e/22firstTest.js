59caa6d735cc3b4456e121f416cbe1aeb1943239
Marek Szumny <lukmarcus@gmail.com>
2023-04-16 03:25:08 +0200

    tests fixes

--- cypress/e2e/22firstTest.js
+++ cypress/e2e/22firstTest.js
@@ -1,8 +1,8 @@
 describe('Heading text', () => {
-    it('Should contain the correct title', () => {
-        cy.visit('/example-1')
-        cy.get('h1')
-            .invoke('text')
-            .should('eq', 'My Awesome Web Application')
-    })
+  it('Should contain the correct title', () => {
+    cy.visit('/example-1')
+    cy.get('h1')
+      .invoke('text')
+      .should('eq', 'My Awesome Web Application')
+  })
 })
